e0d5ab9b3578a6423de28f697c9216d5e0539cea
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2016-10-24 17:14:45 -0400

    Update vue.js
    Added functionality for deleting/removing a single to do item

--- vue.js
+++ vue.js
@@ -1,19 +1,23 @@
 new Vue({
-	el: "#todo",
+  el: "#todo",
   data: {
-  	newTask: "",
+    newTask: "",
     taskList: []
   },
   methods: {
-  	addTask: function() {
-    	var task = this.newTask.trim();
+    addTask: function() {
+      var task = this.newTask.trim();
       if (task) {
-      	this.taskList.push({
-        	text: task,
+        this.taskList.push({
+          text: task,
           checked: false
         });
         this.newTask = "";
       }
+    },
+    removeTask: function(task) {
+    	var index = this.taskList.indexOf(task);
+      this.taskList.splice(index, 1);
     }
   }
 });
